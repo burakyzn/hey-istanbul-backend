562529cb781dfbcc24bcdb1a1ac10d956b823b0e
burakyzn <brkyznn@gmail.com>
2021-12-04 15:05:57 +0300

    refactor comment service

--- Services/CommentService.cs
+++ Services/CommentService.cs
@@ -1,8 +1,10 @@
 using System;
 using hey_istanbul_backend.Data;
+using hey_istanbul_backend.Data.Entities;
 using hey_istanbul_backend.Models;
 using hey_istanbul_backend.Models.Comments;
 using hey_istanbul_backend.Services.Interfaces;
+using System.Linq;
 
 namespace hey_istanbul_backend.Services
 {
@@ -16,22 +18,58 @@ namespace hey_istanbul_backend.Services
 
         public ResultModel<object> CreateComment(CreateCommentRequest request)
         {
-            throw new NotImplementedException();
+            CommentEntity newComment = new CommentEntity{
+                LocationId = request.LocationId,
+                Title = request.Title,
+                Description = request.Description,
+                UserId = request.UserId
+            };
+
+            _dbContext.Add(newComment);
+            _dbContext.SaveChanges();
+
+            return new ResultModel<object>(data: newComment, message: "The comment has been created.");
         }
 
         public ResultModel<object> DeleteComment(Guid commentId, Guid userId)
         {
-            throw new NotImplementedException();
+            CommentEntity comment = _dbContext.Comments
+                .Where(com => com.Id == commentId)
+                .Where(com => com.UserId == userId)
+                .Where(com => com.IsActive)
+                .SingleOrDefault();
+            
+            if(comment == null)
+                return new ResultModel<object>(message: "The comment couldn't be found!", type: ResultModel<object>.ResultType.FAIL);
+
+            comment.IsActive = false;
+
+            _dbContext.Update(comment);
+            _dbContext.SaveChanges();
+
+            return new ResultModel<object>(data: comment, message: "The comment has been removed");
         }
 
         public ResultModel<object> GetCommentByLocationId(string locationId)
         {
-            throw new NotImplementedException();
+            var commentList = _dbContext.Comments
+                .Where(com => com.LocationId == locationId)
+                .Where(com => com.IsActive)
+                .OrderBy(com => com.Created)
+                .ToList();
+            
+            return new ResultModel<object>(data : commentList);
         }
 
         public ResultModel<object> GetCommentByUserId(Guid userId)
         {
-            throw new NotImplementedException();
+            var commentList = _dbContext.Comments
+                .Where(com => com.UserId == userId)
+                .Where(com => com.IsActive)
+                .OrderBy(com => com.Created)
+                .ToList();
+            
+            return new ResultModel<object>(data : commentList);
         }
     }
 }
